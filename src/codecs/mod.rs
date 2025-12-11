81327d74fc2c549b30690abc9eceadd4934edfaf
Yazalde Filimone <yazaldefilimon@gmail.com>
2025-12-11 14:12:06 +0200

    feat(codecs): export ADPCM and RawVideo codecs

--- src/codecs/mod.rs
+++ src/codecs/mod.rs
@@ -1,3 +1,7 @@
+pub mod adpcm;
 pub mod pcm;
+pub mod rawvideo;
 
+pub use adpcm::{AdpcmDecoder, AdpcmEncoder};
 pub use pcm::{PcmDecoder, PcmEncoder};
+pub use rawvideo::{RawVideoDecoder, RawVideoEncoder};
